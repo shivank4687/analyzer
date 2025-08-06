b0986c96e455a27630cc2fabdde7e2476a9ba0c5
Shiwank <shiwank@MacBookPro.lan>
2025-08-06 20:40:15 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import { ChromeService } from './chrome.service';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+    selector: 'app-root',
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
   title = 'stock-analysis-extension';
